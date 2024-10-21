cfcc66f62561714b2ecad5e5b3852dac822af30d
AnderCampos214782 <ander.tadeo10@gmail.com>
2024-10-21 09:10:31 -0600

    Add files via upload

---
+++ P01Variables1.php
@@ -0,0 +1,30 @@
+<?php
+/*  CBTIS 89
+	P01Variables1.php
+	Programa que suma dos números enteros
+	Ander Tadeo Campos Ubiarco
+	3°A Programación Matutino
+	*/
+
+echo "Práctica de Variables con php";echo"<br>"	;
+$dia= 24;
+//declara variables integer
+$sueldo= 234.69;
+//declara variable tipos double
+$nombre= "Pepe";
+//declara variable tipo string
+$exite= true;
+//declara variable tipo boolean
+
+echo "Variable  entera: ";
+echo $dia;echo"<br>";
+echo "Variable double: ";
+echo $sueldo;echo"<br>";
+echo "Variable string: ";
+echo $nombre;echo"<br>";
+echo "Variable boolean: ";
+echo $exite;echo"<br>";
+$cadena1= "Hola";
+$cadena2= "Pibes";
+echo $cadena1." ".$cadena2;
+?>
